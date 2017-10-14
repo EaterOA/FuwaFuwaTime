9351667473869b7c458a5f094c0c781cda2ca1ad
Vincent Wong <duperduper@ucla.edu>
2017-10-14 01:16:18 -0700

    Rearrange AboutDrawer tags to work around firefox bug

--- src/AboutDrawer.js
+++ src/AboutDrawer.js
@@ -26,7 +26,9 @@ class AboutDrawer extends Component {
         <h2>About</h2>
         <p><strong>FuwaFuwaTime</strong>* is an interactive callguide for Love Live! songs. See <a href="https://www.reddit.com/r/LoveLive/comments/4ac21a/beginners_guide_to_basic_concert_callschant/">this</a> for an explanation for what calls are.</p>
         <p className="small">(*No relationship to the popular song of the same name, other than the creator thought it was a clever pun but now regrets that this app is no longer Googleable)</p>
-        <p>Note that in reality there is no definitive set of calls that everyone follows to the letter - this interactive callguide only presents one version for each song that hits most of the common calls. You and the other livers in the concert or live viewing may do something minorly or majorly different <span className="small">(as long as you don't taiga)</span>.</p>
+        <p>
+        Note that in reality there is no definitive set of calls that everyone follows to the letter - this interactive callguide only presents one version for each song that hits most of the common calls. You and the other livers in the concert or live viewing may do something minorly or majorly different.</p>
+        <p className="small">(As long as you don't taiga)</p>
         <h2>Contact</h2>
         <p>Direct all bug reports, questions, concerns, and complaints to <a href="https://www.reddit.com/user/gacha4life/"><strong>gacha4life</strong></a></p>
         <h2>Thanks</h2>
