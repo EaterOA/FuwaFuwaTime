8a2dc9715b2f88a9cdfda4a3a6b9c2fcab9df612
Vincent Wong <duperduper@ucla.edu>
2018-04-27 10:52:28 -0700

    Explicitly unregister service worker
    Turns out service worker still lingered from days ages past...

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js'
 //import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 //registerServiceWorker();
+unregister();
