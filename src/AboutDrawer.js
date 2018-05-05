7dca59dd17a49a8ac0ec0600d41a9c32c2a6c5e1
Vincent Wong <duperduper@ucla.edu>
2018-05-04 22:07:59 -0700

    Minor rewording in AboutDrawer

--- src/AboutDrawer.js
+++ src/AboutDrawer.js
@@ -27,8 +27,7 @@ class AboutDrawer extends Component {
         <p><strong>FuwaFuwaTime</strong>* is an interactive callguide for Love Live! songs. See <a href="https://www.reddit.com/r/LoveLive/comments/4ac21a/beginners_guide_to_basic_concert_callschant/">this</a> for an explanation for what calls are.</p>
         <p className="small">(*No relationship to the popular song of the same name, other than the creator thought it was a clever pun but now regrets that this app is no longer Googleable)</p>
         <p>
-        Note that in reality there is no definitive set of calls that everyone follows to the letter - this interactive callguide only presents one version for each song that hits most of the common calls. You and the other livers in the concert or live viewing may do something minorly or majorly different.</p>
-        <p className="small">(As long as you don't yakkai)</p>
+        Note that in reality there is no definitive set of calls that everyone follows to the letter - this interactive callguide only presents one version for each song that hits most of the common calls. You and the other livers in the concert or live viewing may do something minorly or majorly different. Simply make sure not to be overly disruptive and avoid injuring others - everyone is there to have fun and to enjoy the show.</p>
         <h2>Contact</h2>
         <p>Direct all bug reports, questions, concerns, and complaints to <a href="https://www.reddit.com/user/gacha4life/"><strong>gacha4life</strong></a></p>
         <h2>Thanks</h2>
