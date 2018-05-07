c76f7a5b4f94ab026372dd0b31220789ddee4ab3
Vincent Wong <duperduper@ucla.edu>
2018-05-06 17:47:01 -0700

    sound: no longer use ogg
    Seems like mp3 is more universally supported... as long as the track is
    CBR?

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -12,7 +12,6 @@ class AudioPlayer extends PureComponent {
         controls
         ref={(element) => { this.audioEl = element; }}
       >
-        { this.props.ogg ? <source src={this.props.ogg} type='audio/ogg'/> : null }
         { this.props.mp3 ? <source src={this.props.mp3} type='audio/mp3'/> : null }
       </audio>
     );
