003f6fd5b913c057bce5bc2bcfb8932af838507e
Vincent Wong <duperduper@ucla.edu>
2018-06-24 01:37:27 -0700

    changelog: added 6 3L songs

--- src/AboutDrawer.js
+++ src/AboutDrawer.js
@@ -71,6 +71,7 @@ class AboutDrawer extends Component {
           <Tab label="Changelog"><div className="info-page">
             <h2>Changelog</h2>
             <ul className="changelog-list">
+              <li><strong>2018-06-24</strong>: Added Mirai no Bokura wa Shitteru yo, MY Mai☆TONIGHT, MIRACLE WAVE, Awaken the power, WATER BLUE NEW WORLD, and WONDERFUL STORIES</li>
               <li><strong>2018-06-05</strong>: Corrected bits of romaji and timing in various places</li>
               <li><strong>2018-05-22</strong>: Added Tokimeki Bunruigaku and Yozora wa Nandemo Shitteru no?</li>
               <li><strong>2018-05-09</strong>: Added <a href="https://goo.gl/forms/lytAFNDustx5ZswF2">anonymous survey link</a></li>
