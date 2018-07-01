1c3cf179f2bcfed22e12e5c8d446e216cc009c12
Vincent Wong <duperduper@ucla.edu>
2018-06-30 22:02:29 -0700

    SFXManager: add channels back
    Only two channels needed, but apparently still needed

--- src/SFXManager.js
+++ src/SFXManager.js
@@ -1,21 +1,28 @@
 import pizzicato from 'pizzicato';
 
 class SFXManager {
-  constructor(path, volume=1.0) {
-    this.sfx = new pizzicato.Sound({
-      source: 'file',
-      options: {
-        path: path,
-        volume: volume,
-        attack: 0,
-      },
-    });
+  constructor(path, volume=1.0, channels=2) {
+    this.idx = 0;
+    this.channels = [];
+    for (let i = 0; i < channels; i++) {
+      this.channels.push(new pizzicato.Sound({
+        source: 'file',
+        options: {
+          path: path,
+          volume: volume,
+          attack: 0,
+        },
+      }));
+    }
   }
   updateVolume(volume) {
-    this.sfx.volume = volume;
+    for (let channel in this.channels) {
+      channel.volume = volume;
+    }
   }
   play() {
-    this.sfx.play();
+    this.channels[this.idx].play();
+    this.idx = (this.idx+1) % this.channels.length;
   }
 }
 
